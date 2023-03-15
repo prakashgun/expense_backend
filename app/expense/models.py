6c9c8f849a8e83ea97f336d6e6d2bf9ac4464e98
prakashgun <mail.prakash.india@gmail.com>
2023-03-15 09:36:30 +0530

    Account model added

--- app/expense/models.py
+++ app/expense/models.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class Account(models.Model):
@@ -8,5 +9,6 @@ class Account(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     initial_balance = models.FloatField()
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
