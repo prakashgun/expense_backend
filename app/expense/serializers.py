abec13d929f58f9ee703650d48d15286c27bccb0
prakashgun <mail.prakash.india@gmail.com>
2023-05-23 19:11:49 +0530

    Add account id in api response

--- app/expense/serializers.py
+++ app/expense/serializers.py
@@ -7,7 +7,7 @@ from .models import Account
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = ('name', 'initial_balance', 'owner')
+        fields = ('id', 'name', 'initial_balance', 'owner')
         validators = [UniqueTogetherValidator(
             queryset=Account.objects.all(),
             fields=('name', 'owner'),
